3b9b6965ff8af4c60f18647149391f9c8ead0c31
bondhonshahriaralam <36531785+bondhonshahriaralam@users.noreply.github.com>
2020-02-14 11:07:03 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('a40c7cc9c7dc410282c10172b6ce02de');
+app.set('view engine', 'pug');
+
+newsapi.v2.topHeadlines({
+  sources:"",
+  q: '',
+  category: 'business',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  app.get('/', function(req, res){
+    res.render('index',{title: JSON.stringify(response["totalResults"]), paragraf: JSON.stringify(response)})
+});
+});
+app.listen(3000);
